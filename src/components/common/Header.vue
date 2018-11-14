ce148cecf23a8388ffbec18f63fcc36998949b2c
Hystar <97363829@qq.com>
2018-11-14 15:51:06 +0800

    2018-11-14 commit

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -48,7 +48,7 @@
     export default {
         data() {
             return {
-                collapse: false,
+                collapse: true,
                 fullscreen: false,
                 name: 'linxin',
                 message: 2
